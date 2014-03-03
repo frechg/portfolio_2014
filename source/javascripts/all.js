4b96f14aa23bbcd53d4b92f402f8cdb1241b70ab
Galen Frechette <galenfrechette@gmail.com>
2014-03-02 20:10:34 -0800

    Clean up animation mess and add final project images

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,14 @@
 //= require jquery
 //= project-detail-modal
+//= waypoints.min
 //= require_tree .
+
+$(document).ready(function() {
+  $('article.piece').waypoint(function(down) {
+    // $(this).addClass('animate');
+  }, { offset: '80%' });
+});
+
+$(window).load(function() {
+  $('header').addClass('show');
+});
