7237506804e8ef7d331fc7907bf0c89792384817
Galen Frechette <galenfrechette@gmail.com>
2014-03-28 13:42:56 -0700

    Genearl UI clean up

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,14 +1,6 @@
 //= require jquery
-//= project-detail-modal
-//= waypoints.min
 //= require_tree .
 
 $(document).ready(function() {
-  $('article.piece').waypoint(function(down) {
-    // $(this).addClass('animate');
-  }, { offset: '80%' });
-});
-
-$(window).load(function() {
-  $('header').addClass('show');
+  $('a.view-gallery').smoothScroll();
 });
